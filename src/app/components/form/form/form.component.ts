d6b51e2e871384504406580b2e7849b03ebd6e0a
Lucas <lucasAngular@hotmail.com>
2022-02-09 09:46:25 -0300

    correçao da condcional e do html tambem

--- src/app/components/form/form/form.component.ts
+++ src/app/components/form/form/form.component.ts
@@ -23,49 +23,81 @@ export class FormComponent implements OnInit {
   submitted = false;
   id: number;
   x: any;
-  mostrarInput:boolean = false;
+  mostrarInput: boolean = false;
+  mostrarInputB:boolean = true;
+
   constructor(
     private fb: FormBuilder,
     private service: GrujaService,
     private router: Router,
     private route: ActivatedRoute,
     private modal: AlertService
-    
+
   ) { }
 
   ngOnInit() {
+    const xz = this.route.snapshot.params['id_gruja'];
+
+    this.service.carregaPeloId(xz).subscribe(
+      (data) => {
+        this.grujas = data['id_gruja']
+
+
+      }
+
+    )
+
+    this.route.params.subscribe(
+      (paramns: any) => {
+        const id = paramns['id_gruja']
+        console.log(id);
+        const resultado$ = this.service.carregaPeloId(id);
+        resultado$.subscribe(
+          dados => {
+            this.upDateForm(dados)
+            console.log(dados)
+          }
+        )
+      }
+    )
+
+
+
+
     this.service.list().subscribe(data => this.grujas = data.gorjetas);
     this.listaColaborador();
 
 
     const gorjeta = this.route.snapshot.data['gorjeta']
-    
-   if (this.route.snapshot.params['id_gruja'] > 0){
-          this.mostrarInput = true;
-   }
-   
-     this.form = this.fb.group(
+
+    if (this.route.snapshot.params['id_gruja'] > 0) {
+      this.mostrarInput = true;
+      this.mostrarInputB = false;
+    }
+
+    this.form = this.fb.group(
       {
 
-        id_gruja: [gorjeta.id_gruja, Validators.required],
-        id_colaborador: [gorjeta.id_colaborador, Validators.required],
-        valor: [gorjeta.valor, Validators.required],
-        data: [gorjeta.data, Validators.required]
+        id_gruja: [gorjeta['id_gruja']],
+        id_colaborador: [gorjeta['id_colaborador']],
+        valor: [gorjeta['valor'], Validators.required],
+        data: [gorjeta['data'], Validators.required]
       })
 
-    console.log(this.form.valueChanges)
 
-    
-    
+    console.log(this.form.value.id_gruja)
+
+
+
 
 
   }
 
 
-  analizeInput(){
-    if(this.form.value.id_gruja){
-      this.mostrarInput =false;
-      
+  analizeInput() {
+    if (this.form.value.id_gruja) {
+      this.mostrarInput = false;
+
     }
   }
 
@@ -81,42 +113,52 @@ export class FormComponent implements OnInit {
 
   }
 
+  upDateForm(gorjeta: Gruja[]) {
+    this.form.patchValue({
+      id_gruja: gorjeta['id_gruja'],
+      id_colaborador: gorjeta['id_colaborador'],
+      valor: gorjeta['valor']
+    })
+
+  }
 
   onSubmit() {
     this.submitted = true;
 
     console.log(this.form.value)
     console.log(this.form.value.id_gruja)
+
     if (this.form.valid) {
 
       console.log('submit');
-      let msgSuccess = 'Curso criado com sucesso!';
-      let msgError = 'Erro ao criar curso, tente novamente!';
+
 
       if (this.form.value.id_gruja) {
-        
-        this.service.upDate(this.form.value).subscribe(
-          sucesso => this.modal.sucess('Update', 'Atualizado com sucesso '),
 
+        this.service.upDate(this.form.value).subscribe(
+          (sucesso) => {
+            this.modal.sucess('Update', 'Atualizado com sucesso '),
+              this.router.navigate(['dashboard/Tips'])
+          }
 
         )
       }
 
 
 
-    } else {
-      this.service.create(this.form.value).subscribe(
-
-        sucesses => this.router.navigate(['dashboard/Tips'])
-        ,
-
-        error => console.error(error),
-
-        () => console.log('request ok')
-
-      )
-
     }
+    this.service.create(this.form.value).subscribe(
+
+      sucesses => this.router.navigate(['dashboard/Tips'])
+      ,
+
+      error => console.error(error),
+
+      () => console.log('request ok')
+
+    )
+
+
   }
 
 
