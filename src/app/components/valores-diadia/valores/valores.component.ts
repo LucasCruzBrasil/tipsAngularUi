478ff55ac08f7e13e1c6a8206ae5e9ae85301f45
Lucas <lucasAngular@hotmail.com>
2022-02-11 20:17:06 -0300

    inserindo valores via post funcionando

--- src/app/components/valores-diadia/valores/valores.component.ts
+++ src/app/components/valores-diadia/valores/valores.component.ts
@@ -1,6 +1,8 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 import { Valores } from 'src/app/model/valor';
+import { AlertService } from 'src/app/service/alert.service';
 import { ValoresServiceService } from 'src/app/service/valores-service.service';
 
 @Component({
@@ -10,34 +12,55 @@ import { ValoresServiceService } from 'src/app/service/valores-service.service';
 })
 export class ValoresComponent implements OnInit {
 
-  valores:Valores
+  valores: Valores[];
   formValores: FormGroup
+  public paginaAtual = 1;
+  constructor(private fb: FormBuilder, private service: ValoresServiceService, private http: HttpClient, private alertService: AlertService) {
 
-  constructor(private fb: FormBuilder, private service:ValoresServiceService) { }
-   
+
+  }
+
+
+
+
   ngOnInit(): void {
-     this.service.listaValores().subscribe(
-          data => this.valores = data.valores
-     )
-
-
-    this.formValores = this.fb.group({
-      valor_cartao: [null],
-      valor_dinheiro: [null],
-      valor_pix:[null],
-      valor_pic_pay:[null],
-      valor_data:[null],
-      pessoas_qtd:[null],
+    
+    this.formValores = new FormGroup({
+      valor_cartao: new FormControl(''),
+      valor_dinheiro:new FormControl(''),
+      valor_pix:new FormControl(''),
+      valor_pic_pay:new FormControl(''),
+      data_valor:new FormControl(''),
+      qtd_pessoas:new FormControl('')
 
     })
 
-    console.log(this.formValores.value)
+    this.service.listaValores().subscribe(
+      valores => this.valores = valores.valores
+    )
+
+
+
+
   }
 
+  
   salvarValores(){
-    
+    console.log(this.formValores.value);
+    this.service.salvarValor(this.formValores.value).subscribe(
+    res =>  console.log('Post created successfully!')
+    )
   }
 
 
-      
-}
+  }
+
+
+  
+
+
+
+
+
+
+
