10a8d97a1759fb8e7b849607997dbfbc525d6daa
Lucas <lucasAngular@hotmail.com>
2022-04-04 22:19:12 -0300

    agora tem que ir

--- src/app/service/valores-service.service.ts
+++ src/app/service/valores-service.service.ts
@@ -23,7 +23,7 @@ export class ValoresServiceService {
   }
 
   listaValores() {
-    return this.http.get<Valores>(this.URL + '/valores').pipe(tap(console.log))
+    return this.http.get<Valores>('https://app-lucaback-end.herokuapp.com/valores').pipe(tap(console.log))
 
     
   }
