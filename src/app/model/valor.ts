d5eb27b2719011c8e0216eaaca16d61ec2aa4a3c
Lucas <lucasAngular@hotmail.com>
2022-04-18 21:18:24 -0300

    testando tabela do material

--- src/app/model/valor.ts
+++ src/app/model/valor.ts
@@ -1,13 +1,14 @@
+import { Data } from "@angular/router"
 import { DateAdapter } from "chart.js"
 import { DateFormatter } from "ngx-bootstrap/datepicker"
 
-export interface Valores {
+export class Valores {
     id_valor:number
     valor_cartao:number
     valor_dinheiro:number
     valor_pix:number
     valor_pic_pay:number
-    data_valor:Date
+    data_valor:string = new Date().toISOString()
     qtd_pessoas:number
     total:number
     valor_individual:number
