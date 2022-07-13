11fc647b7642fc262c87af6b562db761fc744357
Lucas <lucasAngular@hotmail.com>
2022-07-13 20:16:50 -0300

    front-end integração mercadopago

--- src/app/service/pix.service.ts
+++ src/app/service/pix.service.ts
@@ -1,9 +1,40 @@
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { throwError } from 'rxjs';
+import { tap } from 'rxjs/operators';
+import { Pix } from '../model/pix';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PixService {
 
-  constructor() { }
+  pix: Pix;
+  
+  public URL = 'https://api-integracao-mercadopago.herokuapp.com'
+
+  constructor(private http: HttpClient) { }
+
+  QrServer(pix: Pix) {
+    return this.http.post(this.URL + '/process_payment', pix).pipe(
+      tap(console.log)
+    )
+  }
+ 
+ 
+ 
+ 
+ 
+  handleError(error: HttpErrorResponse) {
+    let errorMessage = '';
+    if (error.error instanceof ErrorEvent) {
+      // Erro ocorreu no lado do client
+      errorMessage = error.error.message;
+    } else {
+      // Erro ocorreu no lado do servidor
+      errorMessage = `Código do erro: ${error.status}, ` + `menssagem: ${error.message}`;
+    }
+    console.log(errorMessage);
+    return throwError(errorMessage);
+  };
 }
