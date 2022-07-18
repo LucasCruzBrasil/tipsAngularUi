4db950827dd708f56ffc11ac7dd5b72fca48e1ad
Lucas <lucasAngular@hotmail.com>
2022-07-17 23:32:08 -0300

    integração com QR

--- src/app/components/pix-integracao/pix/pix.component.ts
+++ src/app/components/pix-integracao/pix/pix.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { Pix } from 'src/app/model/pix';
+import { responsePix } from 'src/app/model/responsePix';
 import { AlertService } from 'src/app/service/alert.service';
 import { PixService } from 'src/app/service/pix.service';
 
@@ -11,13 +12,13 @@ export interface Ipix {
   description: string;
   payment_method_id: "pix"
   payer: {
-      email: string,
-      first_name: string,
-      last_name: string,
-      identification: {
-          type: "CPF",
-          number: number
-      }
+    email: string,
+    first_name: string,
+    last_name: string,
+    identification: {
+      type: "CPF",
+      number: number
+    }
   }
 }
 
@@ -31,75 +32,80 @@ export interface Ipix {
 
 
 export class PixComponent implements OnInit {
+
   payer: FormGroup
   formPix: FormGroup
-  identification:FormGroup
+  identification: FormGroup
   pix: Pix[];
   m: Ipix[];
+  respostaPix: responsePix
+  qr:responsePix['qrCodeBase64']
+
   constructor(private pixService: PixService, private alertService: AlertService) { }
 
   ngOnInit(): void {
 
     this.formPix = new FormGroup({
 
-
       transaction_amount: new FormControl(''),
       description: new FormControl(''),
       payment_method_id: new FormControl('Pix'),
-      
+
       payer: new FormGroup({
-      email: new FormControl(''),
-      first_name: new FormControl(''),
-      last_name: new FormControl(''),
-      
-      identification: new FormGroup({
-        type: new FormControl('CPF'),
-        number: new FormControl('')
-      })
+        email: new FormControl(''),
+        first_name: new FormControl(''),
+        last_name: new FormControl(''),
+
+        identification: new FormGroup({
+          type: new FormControl('CPF'),
+          number: new FormControl('')
+        })
 
       })
-      
-      
-        
 
-      })
-    
+
+
+
+    })
+
   }
 
-  creatForm(pix: Ipix){
+  creatForm(pix: Ipix) {
     this.formPix = new FormGroup({
-
 
       transaction_amount: new FormControl(''),
       description: new FormControl(''),
       payment_method_id: new FormControl('Pix'),
-      
+
       payer: new FormGroup({
-      email: new FormControl(''),
-      first_name: new FormControl(''),
-      last_name: new FormControl(''),
-      
-      identification: new FormGroup({
-        type: new FormControl('CPF'),
-        number: new FormControl('')
-      })
+        email: new FormControl(''),
+        first_name: new FormControl(''),
+        last_name: new FormControl(''),
+
+        identification: new FormGroup({
+          type: new FormControl('CPF'),
+          number: new FormControl('')
+        })
 
       })
-      
-      
-        
+    })
 
-      })
-    
 
   }
 
   gerarQr(pix: Pix[]) {
     console.log(this.formPix.value);
-     this.m = this.formPix.value;
+    
+    this.m = this.formPix.value;
+    
     this.pixService.QrServer(this.m).subscribe(
 
-    ), (httpError) => {
+      res => {
+        this.respostaPix = res
+        this.qr = this.respostaPix.qrCodeBase64
+        console.log(this.qr); 
+      }
+    ),(httpError) => {
       this.alertService.error(httpError.error.mensagem);
     }
 
---
+++ src/app/model/responsePix.ts
@@ -0,0 +1,10 @@
+import { Pix } from "./pix";
+
+export class responsePix extends Pix {
+    id:number
+    amount: number
+    status: string
+    detail:string
+    qrCodeBase64:string
+
+}
