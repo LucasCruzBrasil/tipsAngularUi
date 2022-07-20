490833ed2f3c4dfb3a3d9847734b781c2c352fbf
Lucas <lucasAngular@hotmail.com>
2022-07-20 17:33:57 -0300

    validação formulario angular

--- src/app/components/pix-integracao/pix/pix.component.ts
+++ src/app/components/pix-integracao/pix/pix.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Pix } from 'src/app/model/pix';
 import { responsePix } from 'src/app/model/responsePix';
 import { AlertService } from 'src/app/service/alert.service';
 import { PixService } from 'src/app/service/pix.service';
+import { Validacoes } from '../validacoes';
 
 
 
@@ -39,7 +40,7 @@ export class PixComponent implements OnInit {
   pix: Pix[];
   m: Ipix[];
   respostaPix: responsePix
-  qr:responsePix['qrCodeBase64']
+  qr: responsePix['qrCodeBase64']
   on: boolean = false;
 
 
@@ -47,20 +48,21 @@ export class PixComponent implements OnInit {
 
   ngOnInit(): void {
 
-    this.formPix = new FormGroup({
+    this.formPix = new FormGroup( {
 
-      transaction_amount: new FormControl(''),
-      description: new FormControl(''),
+      transaction_amount: new FormControl('', [Validators.required]),
+      description: new FormControl('', [Validators.required, Validators.minLength(1)]),
       payment_method_id: new FormControl('Pix'),
 
       payer: new FormGroup({
-        email: new FormControl(''),
-        first_name: new FormControl(''),
-        last_name: new FormControl(''),
+        email: new FormControl('', [Validators.required, Validators.email]),
+
+        first_name: new FormControl('', [Validators.required, Validators.minLength(1)]),
+        last_name: new FormControl('', [Validators.required, Validators.minLength(1)]),
 
         identification: new FormGroup({
           type: new FormControl('CPF'),
-          number: new FormControl('')
+          number: new FormControl('', [Validators.required,Validacoes.ValidaCpf])
         })
 
       })
@@ -95,20 +97,22 @@ export class PixComponent implements OnInit {
 
   }
 
+  
+
   gerarQr(pix: Pix[]) {
     console.log(this.formPix.value);
-    
+
     this.m = this.formPix.value;
-    
+
     this.pixService.QrServer(this.m).subscribe(
 
       res => {
         this.respostaPix = res
         this.on = true;
         this.qr = this.respostaPix.qrCodeBase64
-        console.log(this.qr); 
+        console.log(this.qr);
       }
-    ),(httpError) => {
+    ), (httpError) => {
       this.alertService.error(httpError.error.mensagem);
     }
 
